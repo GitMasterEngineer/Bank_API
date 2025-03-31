7fa267cd854c4e0734b5424c6ec1d38a00e7deca
GitMasterEngineer <anuppochchhi25@gmail.com>
2025-04-01 01:58:20 +0530

    updated Code

--- src/main/java/com/bank/Banking_Application/service/impl/UserServiceImpl.java
+++ src/main/java/com/bank/Banking_Application/service/impl/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.bank.Banking_Application.service.impl;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.stereotype.Service;
 
 import com.bank.Banking_Application.dto.AccountInfo;
 import com.bank.Banking_Application.dto.BankResponse;
+import com.bank.Banking_Application.dto.CreditDebitRequest;
 import com.bank.Banking_Application.dto.EmailDetails;
+import com.bank.Banking_Application.dto.EnquiryRequest;
 import com.bank.Banking_Application.dto.UserRequest;
 import com.bank.Banking_Application.entity.User;
 import com.bank.Banking_Application.repositories.UserRepo;
@@ -45,8 +48,9 @@ public class UserServiceImpl implements UserService {
 
 		// send email alerts
 		EmailDetails emailDetails = EmailDetails.builder().recipient(savedUser.getEmail()).subject("ACCOUNT_CREATION")
-				.messageBody("Congratulations !! Your account has been successfullt created. \n your Account details: \n "+
-						"Account Name:" + savedUser.getFirstName() + " " + savedUser.getLastName() + " "
+				.messageBody(
+						"Congratulations !! Your account has been successfullt created. \n your Account details: \n "
+								+ "Account Name:" + savedUser.getFirstName() + " " + savedUser.getLastName() + " "
 								+ savedUser.getOtherName() + "\n Account Number:" + savedUser.getAccountNumber())
 				.build();
 		emailService.sendEmailAlert(emailDetails);
@@ -61,4 +65,59 @@ public class UserServiceImpl implements UserService {
 
 	}
 
+	@Override
+	public BankResponse balanceEnquiry(EnquiryRequest request) {
+		// check if the provided account number is exists in the DB
+
+		Boolean isAccountExists = userRepo.existsByAccountNumber(request.getAccountNumber());
+		if (!isAccountExists) {
+			return BankResponse.builder().responseCode(Accountutils.ACCOUNT_NOT_EXISTS_CODE)
+					.responseMessage(Accountutils.ACCOUNT_NOT_EXISTS_MESSAGE).accountInfo(null).build();
+
+		}
+		User foundUser = userRepo.findByAccountNumber(request.getAccountNumber());
+		return BankResponse.builder().responseCode(Accountutils.ACCOUNT_FOUND_CODE)
+				.responseMessage(Accountutils.ACCOUNT_FOUND_SUCCESS)
+				.accountInfo(AccountInfo.builder().accountBalance(foundUser.getAccountBalance())
+						.accountNumber(foundUser.getAccountNumber()).accountName(foundUser.getFirstName() + " "
+								+ foundUser.getLastName() + " " + foundUser.getOtherName())
+						.build())
+				.build();
+	}
+
+	@Override
+	public String nameEnquiry(EnquiryRequest request) {
+		Boolean isAccountExists = userRepo.existsByAccountNumber(request.getAccountNumber());
+		if (!isAccountExists) {
+			return Accountutils.ACCOUNT_NOT_EXISTS_MESSAGE;
+	}
+		User foundUser=userRepo.findByAccountNumber(request.getAccountNumber());
+		return foundUser.getFirstName() + " "+foundUser.getLastName() + " "+foundUser.getOtherName();		
+  }
+
+	@Override
+	public BankResponse creditAccount(CreditDebitRequest request) {
+		//checking if account exists
+		boolean isAccountExists=userRepo.existsByAccountNumber(request.getAccountNumber());
+		if(!isAccountExists) {
+			return BankResponse.builder()
+					.responseCode(Accountutils.ACCOUNT_NOT_EXISTS_CODE)
+					.responseMessage(Accountutils.ACCOUNT_NOT_EXISTS_MESSAGE)
+					.accountInfo(null)
+					.build();
+		}
+		User userToCredit = userRepo.findByAccountNumber(request.getAccountNumber());
+		userToCredit.setAccountBalance(userToCredit.getAccountBalance().add(request.getAmount()));
+		userRepo.save(userToCredit);
+		
+		return BankResponse.builder()
+				.responseCode(Accountutils.ACCOUNT_CREDIT_SUCCESS)
+				.responseMessage(Accountutils.ACCOUNT_CREDITED_SUCCESS_MESSAGE)
+				.accountInfo(AccountInfo.builder()
+						.accountName(userToCredit.getFirstName()+" "+userToCredit.getLastName()+ " "+userToCredit.getOtherName())
+						.accountBalance(userToCredit.getAccountBalance())
+						.accountNumber(request.getAccountNumber())
+						.build())
+				.build();
+	}
 }
